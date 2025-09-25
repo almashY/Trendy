ac3f6154908cbade45f817663c262d94917c8e03
almashY <kshow2171@gmail.com>
2025-09-25 21:15:03 +0900

    ジャンル別ランキング表示機能の作成

--- trendyApp/routes.py
+++ trendyApp/routes.py
@@ -1,6 +1,9 @@
 from trendyApp import app
-from flask import render_template
-from trendyApp.services.steam_api import get_top_appids,get_app_detail
+from flask import render_template,request
+from trendyApp.services.steam_api import get_top_appids,get_app_detail,get_genre_top_appids
+
+# 
+limit = 10
 
 @app.route('/')
 def index():
@@ -9,11 +12,18 @@ def index():
 
 @app.route('/ranking')
 def ranking():
-    limit = 10
-
+    
     appids = get_top_appids(limit)
 
     games = [get_app_detail(appid) for appid in appids]
 
     return render_template("ranking.html",games=games)
 
+@app.route('/select_genre', methods=['POST'])
+def select_genre():
+    genre = request.form['genre']  # HTMLのname="genre" で渡された値を取得
+    appids = get_genre_top_appids(limit,genre)
+
+    games = [get_app_detail(appid) for appid in appids]
+    
+    return render_template("ranking.html",games=games)
--- trendyApp/services/steam_api.py
+++ trendyApp/services/steam_api.py
@@ -7,11 +7,44 @@ STEAM_CURRENT_PLAYERS_URL = "https://api.steampowered.com/ISteamUserStats/GetNum
 def get_top_appids(limit):
 
     response = requests.get(STEAM_RANKING_URL)
+    print(response)
     data = response.json()
     games = data["response"]["ranks"][:limit]
-
     return [game["appid"] for game in games]
 
+def get_genre_top_appids(limit,genre):
+
+    response = requests.get(STEAM_RANKING_URL).json()
+    datas = response["response"]["ranks"]
+
+    games = []
+    count = 0
+
+    for data in datas:
+        appid = data["appid"]
+        params = {"appids": appid, "l": "japanese", "cc": "jp"}
+        response_detail = requests.get(STEAM_DETAILS_URL, params=params).json()
+
+        if not response_detail or str(appid) not in response_detail:
+            continue
+
+        app_info = response_detail[str(appid)]
+        if not app_info.get("success") or "data" not in app_info:
+            continue  # data が無い場合はスキップ
+
+        # ジャンルを確認
+        for detail_genre in app_info["data"].get("genres", []):
+            if genre in detail_genre["description"]:
+                games.append(app_info["data"]["steam_appid"])
+                count += 1
+                break
+
+        if count == limit:
+            break
+
+    print(games)
+
+    return games
 
 def get_app_detail(appid):
 
